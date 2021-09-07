0d8afe4103b55452a4869610b1c1e249d12b94a4
raghu vivek <raghu.vivek2003@gmail.com>
2021-09-07 10:45:25 +0530

    Committing vfapigateway to repository

---
+++ vfapigateway/src/main/java/com/vodafone/vfapigateway/VfapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone.vfapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VfapigatewayApplication.class, args);
+	}
+
+}
---
+++ vfapigateway/src/test/java/com/vodafone/vfapigateway/VfapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.vfapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
