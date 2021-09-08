4a1a631ac8b1c6428f256158cb407712ed8d272f
raghu vivek <raghu.vivek2003@gmail.com>
2021-09-08 11:15:49 +0530

    Chekcing in initial version of oauth2

---
+++ oauth2example/src/test/java/com/security/oauth2example/Oauth2exampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.oauth2example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2exampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
